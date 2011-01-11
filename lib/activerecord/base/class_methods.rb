6e826b5aa3afbc275d8efe05beaeab87e046a9cc
Vito Botta <vito@botta.name>
2011-01-11 17:28:27 +0000

    Fixed loading in specs

--- lib/activerecord/base/class_methods.rb
+++ lib/activerecord/base/class_methods.rb
@@ -58,7 +58,8 @@ module Base
       end
     end
     
-    
+
+      
     # 
     # 
     # Overriding the setter for the serialized column in the AR model,
@@ -67,21 +68,32 @@ module Base
     # having the default values, if any, declared in the tableless model itself
     # 
     def default_value_for(property, default_value)
+      
+      # define_method "set_default_value_for_#{property.to_s}" do |*args| 
+      #   return unless new_record? 
+      #   return unless self.respond_to?(property.to_sym)
+      #     
+      #   self.send("#{property.to_s}=".to_sym, self.send(property.to_sym) || default_value)    
+      # end
+      # 
+      # eval "after_initialize \"#{set_default_value_for_#{property.to_s}}\""
+      
+      
       unless method_defined? "after_initialize_with_default_value_for_#{property.to_s}"
-
+      
         unless method_defined? "after_initialize"
           define_method "after_initialize" do |*args|
           end
         end
-
+      
         define_method "after_initialize_with_default_value_for_#{property.to_s}" do |*args| 
           send("after_initialize_without_default_value_for_#{property.to_s}", *args)
           return unless new_record? 
           return unless self.respond_to?(property.to_sym)
-
+      
           self.send("#{property.to_s}=".to_sym, self.send(property.to_sym) || default_value)    
         end
-
+      
         alias_method_chain "after_initialize", "default_value_for_#{property.to_s}"
       end
     end
