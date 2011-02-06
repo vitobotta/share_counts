5cde614ba09904f76cd9943d283dc7dc27b44221
Vito Botta <vito@botta.name>
2011-02-06 00:20:10 +0000

    Added method to fetch Reddit details info for a URL

--- lib/share_counts/reddit.rb
+++ lib/share_counts/reddit.rb
@@ -5,10 +5,9 @@ module ShareCounts
     
     def self.info_for url 
       try("reddit-details", url) {
-        data = extract_info from_json( "http://www.reddit.com/api/info.json", :url => url ), 
-          :selector => "data/children/data" 
-          
-        data.reject!{ |key| 
+        data = extract_info from_json( "http://www.reddit.com/api/info.json", :url => url ), :selector => "data/children/data" 
+
+        data.reject{ |key, value| 
             %w( media_embed levenshtein selftext_html selftext likes saved clicked media over_18
                 hidden thumbnail subreddit_id is_self created subreddit_id created_utc num_comments
                 domain subreddit id author downs name url title ups
