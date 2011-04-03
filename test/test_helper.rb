0d0cd632624f9d3a5273501d33885a3522875bd6
Vito Botta <vito@botta.name>
2011-04-03 17:09:07 +0100

    Added tests for the main methods

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,110 @@ include WebMock::API
 
 require File.join(File.dirname(__FILE__), "../lib/share_counts")
 
+SOME_URL      = "http://vitobotta.com/cv-resume/"
+SOME_PARAMS       = [ :url => "http://vitobotta.com/cv-resume/", :callback => "myCallback" ]
+
+class Array
+  def to_hash
+    @hash ||= self.inject({}){|r, c| r.merge!(c); r }
+  end
+end
+
+class Reddit
+  def self.params
+    [:url => SOME_URL]
+  end
+  def self.api  
+    "http://www.reddit.com/api/info.json"
+  end
+  def self.json
+    @json ||= File.read(File.join(File.dirname(__FILE__), "reddit.json"))
+  end
+  def self.selector
+    "data/children/data/score"
+  end
+end
+
+class Digg
+  def self.api
+    "http://services.digg.com/2.0/story.getInfo"
+  end
+  def self.params
+    [:links => SOME_URL]
+  end
+  def self.json
+    @json ||= File.read(File.join(File.dirname(__FILE__), "digg.json"))
+  end
+  def self.selector
+    "stories/diggs"
+  end
+end
+
+class Twitter 
+  def self.api
+    "http://urls.api.twitter.com/1/urls/count.json"
+  end
+  def self.params
+    [:url => SOME_URL]
+  end
+  def self.json
+    @json ||= File.read(File.join(File.dirname(__FILE__), "twitter.json"))
+  end
+  def self.selector
+    "count"
+  end
+end
+
+class Facebook
+  def self.api
+    "http://api.facebook.com/restserver.php"
+  end
+  def self.json
+    @json ||= File.read(File.join(File.dirname(__FILE__), "facebook.json"))
+  end
+  def self.params
+    [:v => "1.0", :method => "links.getStats", :urls => SOME_URL, :callback => "fb_sharepro_render", :format => "json"]
+  end
+  def self.selector
+    "total_count"
+  end
+end
+
+class Linkedin
+  def self.api
+    "http://www.linkedin.com/cws/share-count"
+  end
+  def self.params
+    [:url => SOME_URL, :callback => "IN.Tags.Share.handleCount"]
+  end
+  def self.json
+    @json ||= File.read(File.join(File.dirname(__FILE__), "linkedin.json"))
+  end
+  def self.selector
+    "count"
+  end
+end
+
+class GoogleBuzz
+  def self.api
+    "http://www.google.com/buzz/api/buzzThis/buzzCounter"
+  end
+  def self.params
+    [:url => SOME_URL, :callback => "google_buzz_set_count"]
+  end
+  def self.json
+    @json ||= File.read(File.join(File.dirname(__FILE__), "googlebuzz.json")) 
+  end
+end
+
+class StumbleUpon
+  def self.api
+    "http://www.stumbleupon.com/badge/embed/5/"
+  end
+  def self.params
+    [:url => SOME_URL]
+  end
+  def self.html
+    @json ||= File.read(File.join(File.dirname(__FILE__), "stumbleupon.html"))  
+  end
+end
--- test/test_share_counts.rb
+++ test/test_share_counts.rb
@@ -1,10 +1,6 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "test_helper"))
 
 class ShareCountsTest < ActiveSupport::TestCase
-  A_REMOTE_URL     = "http://www.someservice.com"
-  SOME_PARAMS      = [ :url => "http://vitobotta.com/cv-resume/", :callback => "myCallback" ]
-  SOME_PARAMS_HASH = SOME_PARAMS.inject({}){|r, c| r.merge!(c); r }
-  
   setup do
     $stderr = @stderr = StringIO.new
     $stdin  = @stdin  = StringIO.new
@@ -12,9 +8,9 @@ class ShareCountsTest < ActiveSupport::TestCase
   end
   
   def teardown
-    $stderr = STDERR
-    $stdin  = STDIN
-    $stdout = STDOUT
+    $stderr = @stderr =  STDERR
+    $stdin  = @stdin  =  STDIN
+    $stdout = @stdout =  STDOUT
   end
   
   test ".supported_networks returns the supported networks" do
@@ -22,16 +18,16 @@ class ShareCountsTest < ActiveSupport::TestCase
   end
   
   test ".make_request makes a request to a remove service and returns the response" do
-    stub_request(:get, A_REMOTE_URL).with(:query => SOME_PARAMS_HASH).to_return(:body => "---RESPONSE---")
+    stub_request(:get, SOME_URL).with(:query => SOME_PARAMS.to_hash).to_return(:body => "---RESPONSE---")
   
-    assert_equal("---RESPONSE---", ShareCounts.send(:make_request, A_REMOTE_URL, *SOME_PARAMS ))
+    assert_equal("---RESPONSE---", ShareCounts.send(:make_request, SOME_URL, *SOME_PARAMS ))
     assert_equal(0, @stdout.string.split("\n").size)
   end
   
   test ".make_request should raise an exception if the remote service returns a 500 status code for three attempts" do
-    stub_request(:get, A_REMOTE_URL).to_return(:status =>  [500, "Internal Server Error"])
+    stub_request(:get, SOME_URL).to_return(:status =>  [500, "Internal Server Error"])
   
-    assert_raise(Exception) { ShareCounts.send(:make_request, A_REMOTE_URL)  }
+    assert_raise(Exception) { ShareCounts.send(:make_request, SOME_URL)  }
     
     errors = []
     3.times {|n| errors << "Failed #{n+1} attempt(s) - 500 Internal Server Error" }
@@ -40,9 +36,9 @@ class ShareCountsTest < ActiveSupport::TestCase
   
   
   test ".make_request should raise an exception if the remote service times out for three attempts" do
-    stub_request(:get, A_REMOTE_URL).to_timeout
+    stub_request(:get, SOME_URL).to_timeout
     
-    assert_raise(Exception) { ShareCounts.send(:make_request, A_REMOTE_URL)  }
+    assert_raise(Exception) { ShareCounts.send(:make_request, SOME_URL)  }
     
     errors = []
     3.times {|n| errors << "Failed #{n+1} attempt(s) - Request Timeout" }
@@ -50,35 +46,123 @@ class ShareCountsTest < ActiveSupport::TestCase
   end
   
   test ".make_request should return response if remote service fails < 3 attempts" do
-    stub_request(:get, A_REMOTE_URL).
+    stub_request(:get, SOME_URL).
       to_return(:status =>  [500, "Internal Server Error"]).then.
       to_timeout.then.
       to_return(:body => "---RESPONSE---" )
     
-    assert_nothing_raised(Exception) { assert_equal("---RESPONSE---", ShareCounts.send(:make_request, A_REMOTE_URL)) }
+    assert_nothing_raised(Exception) { assert_equal("---RESPONSE---", ShareCounts.send(:make_request, SOME_URL)) }
     
     assert_equal(["Failed 1 attempt(s) - 500 Internal Server Error", "Failed 2 attempt(s) - Request Timeout"].sort, @stdout.string.split("\n").sort)
   end
-
+  
   test ".make_request should strip the callback call from the JSON response if a callback has been specified" do
-    stub_request(:get, A_REMOTE_URL).with(:query => SOME_PARAMS_HASH).
+    stub_request(:get, SOME_URL).with(:query => SOME_PARAMS.to_hash).
       to_return(:body => "myCallback(JSON_DATA);").then.
       to_return(:body => "myCallback(JSON_DATA)")
-
-    assert_equal("JSON_DATA", ShareCounts.send(:make_request, A_REMOTE_URL, *SOME_PARAMS ))
-    assert_equal("JSON_DATA", ShareCounts.send(:make_request, A_REMOTE_URL, *SOME_PARAMS ))
+  
+    assert_equal("JSON_DATA", ShareCounts.send(:make_request, SOME_URL, *SOME_PARAMS ))
+    assert_equal("JSON_DATA", ShareCounts.send(:make_request, SOME_URL, *SOME_PARAMS ))
     assert_equal(0, @stdout.string.split("\n").size)
   end
   
   
   test ".from_json parses the JSON response returned by a remote service" do
-    stub_request(:get, A_REMOTE_URL).to_return(:body => "{\"a\":1,\"b\":2}").then.to_return(:body => "[\"a\", \"b\", 1, 2]")
-    stub_request(:get, A_REMOTE_URL).with(:query => SOME_PARAMS_HASH).to_return(:body => "myCallback({\"a\":1,\"b\":2})")
-
-    assert_equal({ "a" => 1, "b" => 2 }, ShareCounts.send(:from_json, A_REMOTE_URL))
-    assert_equal(["a", "b", 1, 2], ShareCounts.send(:from_json, A_REMOTE_URL))
-    assert_equal({ "a" => 1, "b" => 2 }, ShareCounts.send(:from_json, A_REMOTE_URL, *SOME_PARAMS ))
+    stub_request(:get, SOME_URL).to_return(:body => "{\"a\":1,\"b\":2}").then.to_return(:body => "[\"a\", \"b\", 1, 2]")
+    stub_request(:get, SOME_URL).with(:query => SOME_PARAMS.to_hash).to_return(:body => "myCallback({\"a\":1,\"b\":2})")
+  
+    assert_equal({ "a" => 1, "b" => 2 }, ShareCounts.send(:from_json, SOME_URL))
+    assert_equal(["a", "b", 1, 2], ShareCounts.send(:from_json, SOME_URL))
+    assert_equal({ "a" => 1, "b" => 2 }, ShareCounts.send(:from_json, SOME_URL, *SOME_PARAMS ))
     assert_equal(0, @stdout.string.split("\n").size)
+  end
+  
+  test ".extract_info correctly extract the information from the parsed JSON data received, in XPATH style" do
+    teardown
+    
+    stub_request(:get, Reddit.api).with(:query => Reddit.params.to_hash).to_return(:body => Reddit.json)
+    stub_request(:get, Digg.api).with(:query => Digg.params.to_hash).to_return(:body => Digg.json)
+    stub_request(:get, Twitter.api).with(:query => Twitter.params.to_hash).to_return(:body => Twitter.json)
+    stub_request(:get, Facebook.api).with(:query => Facebook.params.to_hash).to_return(:body => Facebook.json)
+    stub_request(:get, Linkedin.api).with(:query => Linkedin.params.to_hash).to_return(:body => Linkedin.json)
+    
+
+    assert_equal(31,  ShareCounts.send(:extract_info, ShareCounts.send(:from_json, Reddit.api, *Reddit.params), { :selector => Reddit.selector } ))
+    assert_equal(1,   ShareCounts.send(:extract_info, ShareCounts.send(:from_json, Digg.api, *Digg.params), { :selector => Digg.selector } ))
+    assert_equal(35,   ShareCounts.send(:extract_info, ShareCounts.send(:from_json, Twitter.api, *Twitter.params), { :selector => Twitter.selector } ))
+    assert_equal(23,   ShareCounts.send(:extract_info, ShareCounts.send(:from_json, Facebook.api, *Facebook.params), { :selector => Facebook.selector } ))
+    assert_equal(23,   ShareCounts.send(:extract_info, ShareCounts.send(:from_json, Linkedin.api, *Linkedin.params), { :selector => Linkedin.selector } ))
+    
+  end
+
+  test ".reddit should return the reddit score" do
+    stub_request(:get, Reddit.api).with(:query => Reddit.params.to_hash).to_return(:body => Reddit.json)
+    assert_equal(31, ShareCounts.reddit(SOME_URL))
+  end
+
+  test ".reddit with raise_exceptions=true should raise exception" do
+    stub_request(:get, Reddit.api).with(:query => Reddit.params.to_hash).to_raise(Exception)
+    assert_raise(Exception) { ShareCounts.reddit(SOME_URL, true) }
+  end
+
+  test ".digg should return the digg score" do
+    stub_request(:get, Digg.api).with(:query => Digg.params.to_hash).to_return(:body => Digg.json)
+    assert_equal(1, ShareCounts.digg(SOME_URL))
+  end
+
+  test ".digg with raise_exceptions=true should raise exception" do
+    stub_request(:get, Digg.api).with(:query => Digg.params.to_hash).to_raise(Exception)
+    assert_raise(Exception) { ShareCounts.digg(SOME_URL, true) }
+  end
+  
+  test ".twitter should return the twitter score" do
+    stub_request(:get, Twitter.api).with(:query => Twitter.params.to_hash).to_return(:body => Twitter.json)
+    assert_equal(35, ShareCounts.twitter(SOME_URL))
+  end
+
+  test ".twitter with raise_exceptions=true should raise exception" do
+    stub_request(:get, Twitter.api).with(:query => Twitter.params.to_hash).to_raise(Exception)
+    assert_raise(Exception) { ShareCounts.twitter(SOME_URL, true) }
+  end
+  
+  test ".facebook should return the facebook score" do
+    stub_request(:get, Facebook.api).with(:query => Facebook.params.to_hash).to_return(:body => Facebook.json)  
+    assert_equal(23, ShareCounts.facebook(SOME_URL))
+  end
+
+  test ".facebook with raise_exceptions=true should raise exception" do
+    stub_request(:get, Facebook.api).with(:query => Facebook.params.to_hash).to_raise(Exception)
+    assert_raise(Exception) { ShareCounts.facebook(SOME_URL, true) }
+  end
+  
+  test ".linkedin should return the linkedin score" do
+    stub_request(:get, Linkedin.api).with(:query => Linkedin.params.to_hash).to_return(:body => Linkedin.json)
+    assert_equal(23, ShareCounts.linkedin(SOME_URL))
+  end
+
+  test ".linkedin with raise_exceptions=true should raise exception" do
+    stub_request(:get, Linkedin.api).with(:query => Linkedin.params.to_hash).to_raise(Exception)
+    assert_raise(Exception) { ShareCounts.linkedin(SOME_URL, true) }
+  end
+  
+  test ".googlebuzz should return the googlebuzz score" do
+    stub_request(:get, GoogleBuzz.api).with(:query => GoogleBuzz.params.to_hash).to_return(:body => GoogleBuzz.json)
+    assert_equal(1, ShareCounts.googlebuzz(SOME_URL))
+  end
+
+  test ".googlebuzz with raise_exceptions=true should raise exception" do
+    stub_request(:get, GoogleBuzz.api).with(:query => GoogleBuzz.params.to_hash).to_raise(Exception)
+    assert_raise(Exception) { ShareCounts.googlebuzz(SOME_URL, true) }
+  end
+  
+  test ".stumbleupon should return the stumbleupon score" do
+    stub_request(:get, StumbleUpon.api).with(:query => StumbleUpon.params.to_hash).to_return(:body => StumbleUpon.html)
+    assert_equal(6, ShareCounts.stumbleupon(SOME_URL))
+  end
+
+  test ".stumbleupon with raise_exceptions=true should raise exception" do
+    stub_request(:get, StumbleUpon.api).with(:query => StumbleUpon.params.to_hash).to_raise(Exception)
+    assert_raise(Exception) { ShareCounts.stumbleupon(SOME_URL, true) }
   end
   
 end
